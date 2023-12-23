ac53b6071711f3418ccde7c19ba4bd2537581251
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2023-12-23 06:06:27 +0530

    nav commit

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,10 +1,23 @@
 function Navbar(){
     return(
-<div class="nav-container">
+        <nav className="bg-red-900 w-full">
+      <div class="nav-container flex">
     <div class="logo">
         <h1>Blog</h1>
     </div>
-</div>
+    <div class="link">
+        <ul className="flex">
+            <li className="visibility: hidden">Product</li>
+            <li className="visibility: hidden">Resourse</li>
+            <li className="visibility: hidden">Education</li>
+            <li className="visibility: hidden">Pricing</li>
+        </ul>
+    </div>
+    <button>Get a Demo</button>
+    <button>Free Trial</button>
+</div>      
+        </nav>
+
 
     )
 }
