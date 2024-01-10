fc031e0aa2e0b84b91daf56f60c4fb413d88bd1e
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-01-10 19:33:14 +0530

    corusal commit

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Navbar from "./components/navbar";
 /*import MultiActionAreaCard from "./components/cardui";*/
 import Making from "./components/cards";
+import UncontrolledExample from "./components/corusal";
 //import Multicard from "./components/multicards";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
    
     <div className="bg-zinc-900">
          <Navbar />
+         <UncontrolledExample/>
 <Making/>
     </div>
    
