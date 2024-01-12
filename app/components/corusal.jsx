fdb677d2890a4386f899bcbb80c3556c11d8dc26
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-01-12 08:35:43 +0530

    second commit

--- app/components/corusal.jsx
+++ app/components/corusal.jsx
@@ -1,25 +1,25 @@
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import image11 from '/public/image11.jpg';
 
 function UncontrolledExample() {
   return (
     <Carousel>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+      <img src={image11} alt="First slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+      <img src={image11} alt="Second slide" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+      <img src={image11} alt="Third slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
